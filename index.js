f34bd8abb67d028151b166459c282e0ada64e044
Ajay Khatri <71509729+its-ajay-khatri@users.noreply.github.com>
2023-01-03 19:26:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,6 +91,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(8000, '127.0.0.1', () => {
+const port = process.env.PORT || 8000;
+
+server.listen(port, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000');
 });
